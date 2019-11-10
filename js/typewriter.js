26f8c621182450f745ea38f8e94701ef7750ed4c
Josh Green <jhgreen@umich.edu>
2019-11-10 18:10:40 -0500

    text

--- js/typewriter.js
+++ js/typewriter.js
@@ -4,7 +4,7 @@ var speed = 90; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("type").innerHTML += txt.charAt(i);
+    document.getElementById("type").innerHTML = txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
